9f9c50b9c36352182438d00e24051b7bd76baab2
JosueB10 <josuebeteta8@gmail.com>
2022-09-11 21:42:36 -0600

    Cambios

---
+++ HolaMundo/src/HolaMundo/HolaMundoDeGitHub.java
@@ -0,0 +1,12 @@
+package HolaMundo;
+
+public class HolaMundoDeGitHub {
+
+	public static void main(String[] args) {
+		System.out.println("Hola mundo de Git Hub :D");
+		System.out.println("Aprendiendo a usar comandos en Git ");
+		System.out.println("Fin del programaaaa");
+
+	}
+
+}
